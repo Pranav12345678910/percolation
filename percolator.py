b82140884b2ce9d92ca3cd0647119623303f952a
Pranav <pstatavarti@gmail.com>
2020-11-17 16:12:29 -0800

    Prerequisites

---
+++ percolator.py
@@ -0,0 +1,7 @@
+import random
+
+def ChooseVertexToColor(graph, player):
+    return random.choice([v for v in graph.V if v.color == -1])
+
+def ChooseVertexToRemove(graph, player):
+    return random.choice([v for v in graph.V if v.color == player])
