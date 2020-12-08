87cb284dbe1ce0c83ad4c67d87e866e957f19f06
Pranav <pstatavarti@gmail.com>
2020-12-08 14:45:33 -0800

    Added new percolator

--- percolator.py
+++ percolator.py
@@ -35,16 +35,40 @@ class PercolationPlayer:
 
 
     def ChooseVertexToRemove(graph, player):
-        #want to minimize isolation of your own vertices    
-        #delete ones that have the least amount of connections to your vertices and the most amount of connections to other vertices
-        #very bad to have several of your own vertices touching
-        color = 0
-        if player == 1:
-            color = 1
-        else:
-            color = 0
-        opposite_colored = [x for x in graph.V if x.color != color]
-        num_color = {i: PercolationPlayer.numVerticesNotOurs(graph, i, player) for i in opposite_colored} 
-        print(num_color.items())
-        best_vertex_pair = sorted(num_color.items(), key=lambda x: x[1], reverse = True)[0]
-        return best_vertex_pair[0]
+        #search two moves ahead, look at every move you can make and then every move the opponent can make if you made that move, 
+        #and see how good the gameboard is. The vertex that ultimately can yield the greatest number of good states after the opponent makes their 
+        #next move is the one we want to pick 
+        
+    '''
+    def pickVertex(graph, player):
+        #picks the most promising vertex to search through (we can't search through every vertex)
+        best_v_num = 0
+        best_v = 0
+        for x in self.V:
+            if len(Neighbors(x, graph)) > best_v_num:
+                best_v_num = len(Neighbors(x, graph))
+                best_v = x
+        return best_v
+    '''
+
+    def Searchthingy(graph, v):
+        frontier = [v]
+        #discovered needs to be a set with tuples that represent 1d states 
+        discovered = set(v)  
+        #state is a 2d tuple already, so since it is hashable I can immediately add it
+        parents = {graph : ()}
+        while len(frontier) != 0:
+            current_state = frontier.pop(0)
+            discovered.add()
+            if isGoal(current_state):
+                #return every key in the dictionary (node) in the order of most recently to last recently placed nodes
+                return parents[tuple(map(tuple, current_state))]
+            neighbors = convertStates(computeNeighbors(current_state))  
+            for neighbor in range(len(neighbors)):
+                active_state = neighbors[neighbor]
+                if tuple(flatten(active_state)) not in discovered:
+                    frontier.append(active_state)
+                    discovered.add(tuple(flatten(active_state)))
+                    new_path = list(parents[(tuple(map(tuple, current_state)))])
+                    new_path.append(computeNeighbors(current_state)[neighbor][0])
+                    parents[tuple(map(tuple, active_state))] = tuple(new_path)
